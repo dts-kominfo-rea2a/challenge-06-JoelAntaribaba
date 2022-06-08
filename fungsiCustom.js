51387eb3753c3080f05df49cc519a083ea51c6d8
JoelAntaribab <joelantaribaba@gmail.com>
2022-06-09 03:06:25 +0900

    perubahan ke-1

--- fungsiCustom.js
+++ fungsiCustom.js
@@ -1,5 +1,5 @@
 // TODO: import module bila dibutuhkan di sini
-
+const fs = require("fs");
 // ! JANGAN DIMODIFIKASI
 let file1 = "./data1.json";
 let file2 = "./data2.json";
@@ -18,7 +18,36 @@ let modifyFile3 = (val) => {
 
 // TODO: Kerjakan bacaData
 // gunakan variabel file1, file2, dan file3
-const bacaData = null;
+
+const bacaData = (fnCallback) => {
+  const newFile1 = fs.readFile(file1, "utf8", (err, data) => {
+    if (err) {
+      console.log(err);
+    } else {
+      const cvtData = JSON.parse(data);
+      cvtData.message.slice(5, 10);
+      console.log(cvtData.message.slice(5, 10));
+    }
+  });
+
+  const newFile2 = fs.readFile(file2, "utf8", (err, data) => {
+    if (err) {
+      console.log(err);
+    } else {
+      const cvtData = JSON.parse(data);
+      console.log(cvtData[0].message.slice(5, 10));
+    }
+  });
+
+  const newFile3 = fs.readFile(file3, "utf8", (err, data) => {
+    if (err) {
+      console.log(err);
+    } else {
+      const cvtData = JSON.parse(data);
+      console.log(cvtData[0].data.message.slice(5, 10));
+    }
+  });
+};
 
 // ! JANGAN DIMODIFIKASI
 module.exports = {
